7a73ce621eaafc86b1cc3bec131fc21867182060
Tristan Elliott <tristanelliott7@gmail.com>
2020-05-28 17:06:48 -0300

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import NavBar from '../NavBar'
+
+const App = ()=>{
+
+  return(
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './index.css'
+
+
+
+const NavBar = ()=>{
+  return(
+    <div>
+      <nav>
+        <ul class="nav__links">
+          <li><a href="/">Home</a></li>
+          <li><a href="/">Cart</a></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
